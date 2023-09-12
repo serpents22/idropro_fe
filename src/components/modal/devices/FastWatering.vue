8dfddba99e6aa4e7dd05a63b4da1ae38cf084e51
serpents22 <ivandarmawan67@gmail.com>
2023-09-12 19:32:18 +0700

    fix: typo

--- src/components/modal/devices/FastWatering.vue
+++ src/components/modal/devices/FastWatering.vue
@@ -16,14 +16,14 @@
                 <p class="col-span-1 text-center">{{$t('plant')}}</p>
                 <p class="col-span-1 text-center">{{ $t('state') }}</p>
               </span>
-              <span v-for="device in fastWateringList" class="grid grid-cols-2 text-start text-white mb-2 border-b border-white">
-                <p class="col-span-1 text-center">{{ device.device_name }}</p>
+              <span v-for="device in devicesList" class="grid grid-cols-2 text-start text-white mb-2 border-b border-white">
+                <p class="col-span-1 text-center">{{ device.name }}</p>
                 <div class="w-full flex justify-center items-center mb-3">
                   <label class="inline-flex relative items-center cursor-pointer">
                     <input 
                     type="checkbox"
-                    v-model="device.S988"
-                    @change="onToggle(device.device,device.S988)"
+                    v-model="device.is_fast_watering"
+                    @change="onToggle(device.code,device.is_fast_watering)"
                     class="sr-only peer">
                     <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                   </label>
@@ -56,6 +56,7 @@ import { storeToRefs } from 'pinia'
   const fastWateringStore = useFastWateringStore()
   const { fastWateringList } = storeToRefs(useFastWateringStore())
   const devicesStore = useDevicesStore()
+  const { devicesList } = storeToRefs(useDevicesStore())
 
 
   const fastWateringCommand = ref({
