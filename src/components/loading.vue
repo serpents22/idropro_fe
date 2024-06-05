26528daff7b2360ec978ffb8b0cac205c92b217c
serpents22 <ivandarmawan67@gmail.com>
2024-06-05 20:19:50 +0700

    update: loading

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -64,7 +64,7 @@ export default {
 .appLoading__backdrop {
   @apply  
         /* bg-[#ABADAF]/40 backdrop-blur-[2px] */
-        bg-[#ADCBF6]
+        bg-white
           fixed top-0 bottom-0 left-0 right-0
           flex justify-center items-center 
 }
